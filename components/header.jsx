31baceed99405ec48426f71514bda1866996aeb2
Ashen1919 <2303S0834@saegis.ac.lk>
2025-06-13 21:20:08 +0530

    install tailwind css and config

---
+++ components/header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <div className="w-full h-[70px] bg-blue-600 p-5 items-center">
+            <h1 className="text-2xl font-bold text-white">To-Do-List</h1>
+        </div>
+    )
+}
