1333ba591ae3ba84fa622c56e0a579b3a1749c94
Ashen1919 <2303S0834@saegis.ac.lk>
2025-06-14 20:30:11 +0530

    header designed

--- components/header.jsx
+++ components/header.jsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return(
-        <div className="w-full h-[70px] bg-blue-600 p-5 items-center">
+        <div className="w-full h-[70px] bg-gray-800 p-5 items-center flex justify-between">
             <div>
                 <a href="/"><h1 className="text-2xl font-bold text-white">To-Do-List</h1></a>
             </div>
+            <div className="max-w-full">
+                <Link className="text-white text-xl hover:underline mr-6.5" to={'/'}>Home</Link>
+                <Link className="text-white text-xl hover:underline" to={'/list'}>To-Do-List</Link>
+            </div>
             <div>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/list'}>To-Do-List</Link>
+                <Link className="text-white text-xl hover:underline mr-6.5" to={'/login'}>Login</Link>
+                <Link className="text-white text-xl hover:underline" to={'/signup'}>SignUp</Link>
             </div>
             
         </div>
--- pages/homePage.jsx
+++ pages/homePage.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/header";
+import HomeContent from "./homeContent";
 
 export default function HomePage() {
     return(
         <div>
             <Header/>
+            <HomeContent/>
         </div>
     )
 }
