2428f55efe44e6a7615a54ecf82590534fa3238a
Ashen1919 <2303S0834@saegis.ac.lk>
2025-06-14 12:55:46 +0530

    update code

--- components/header.jsx
+++ components/header.jsx
@@ -3,5 +3,6 @@ export default function Header() {
         <div className="w-full h-[70px] bg-blue-600 p-5 items-center">
             <h1 className="text-2xl font-bold text-white">To-Do-List</h1>
         </div>
+        
     )
 }
