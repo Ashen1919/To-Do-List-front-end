a8fe353a6d5f3ee06d9de3fde5786a5988b44618
Ashen1919 <2303S0834@saegis.ac.lk>
2025-06-14 15:33:22 +0530

    Initialize react-router-dom and create path routers

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return(
         <div className="w-full h-[70px] bg-blue-600 p-5 items-center">
-            <h1 className="text-2xl font-bold text-white">To-Do-List</h1>
+            <div>
+                <a href="/"><h1 className="text-2xl font-bold text-white">To-Do-List</h1></a>
+            </div>
+            <div>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/list'}>To-Do-List</Link>
+            </div>
+            
         </div>
         
     )
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import Header from "../components/header"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "../pages/homePage"
+import Login from "../pages/login"
+import SignUp from "../pages/signUp"
+import ListPage from "../pages/listPage"
 
 function App() {
 
   return (
-    <>
-      <Header/>
-    </>
+    <BrowserRouter>
+      <Routes path="/*">
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<SignUp/>}></Route>
+        <Route path="/list" element={<ListPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
